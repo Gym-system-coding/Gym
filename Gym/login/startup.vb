a6a6d39e72eb2d0f07cb1e97eedca0a98069ec1f
Uni-Mar <987601970@qq.com>
2020-11-17 22:35:58 +0800

    loginUI

--- Gym/login/startup.vb
+++ Gym/login/startup.vb
@@ -1,3 +1,8 @@
-﻿Public Class startup
-
+﻿Imports System.Drawing
+Imports System.Windows.Forms
+Public Class startup
+    Private Sub login_LinkClicked(sender As Object, e As Windows.Forms.LinkLabelLinkClickedEventArgs) Handles loginlink.LinkClicked
+        Dim lg As Form = New login
+        lg.Show()
+    End Sub
 End Class
