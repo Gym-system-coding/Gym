a309fb447ebdff5af82f284b3a121404abcd3f2e
Uni-Mar <987601970@qq.com>
2020-11-29 19:44:00 +0800

    delete

--- Gym/UI/teacher-UI.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports System.Windows.Forms
-
-Public Class teacher_UI
-    Private Sub teacher_UI_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Me.WindowState = FormWindowState.Maximized
-    End Sub
-End Class
